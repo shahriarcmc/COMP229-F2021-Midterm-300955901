10e8c96dcfc843879a4380b046e3dc494eb01456
Shahriar Moinuddin <shahriar.cmc@gmail.com>
2021-10-28 00:29:10 -0400

    Atlas DB added

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
 module.exports = {
     "AtlasDB": "mongodb+srv://shahriar:OUCHovv38Gt0gkLP@mongodbserver.do6ff.mongodb.net/books?retryWrites=true&w=majority"
-    // "AtlasDB": "mongodb://localhost/contactlist"
 }
